e92d10b77e77569a9cb5c508eb4cffed6b04c9f9
Krzysiek <boduch.krzysztof@gmail.com>
2017-05-24 15:42:22 +0200

    gst

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,24 @@
 var gulp = require('gulp'),
-    dist = './dist', src = './src',
+    rootPath = './',
+    dist = rootPath + 'dist',
+    src = rootPath + 'src',
+
+    distAssetPath = dist + '/assets',
+    jsDistAssetPath = distAssetPath + '/js',
+    cssDistAssetPath = distAssetPath + '/css',
+    fontsDistAssetPath = distAssetPath + '/fonts',
+
     compileTwigSrc = [src + '/**/*.twig', '!' + src + '/**/_*.twig'],
-    watchTwigSrc = [src + '/**/*.twig', './twigConfig.js']
-;
+    watchTwigSrc = [src + '/**/*.twig', rootPath + 'twigConfig.js'],
+
+    watchBowerLibSrc = [rootPath + 'bower.json'],
+    env = (process.argv.indexOf("--prod") != -1) ? 'production' : 'development'
+    ;
 
 gulp.task('compile-twig', function () {
     'use strict';
     var twig = require('gulp-twig'),
-        twigConfig = require('./twigConfig');
+        twigConfig = require(rootPath + 'twigConfig');
 
     return gulp.src(compileTwigSrc)
         .pipe(twig({
@@ -26,20 +37,42 @@ gulp.task('purge-dist', function () {
         .pipe(clean());
 });
 
+gulp.task('main-bower-files', function () {
+    var mainBowerFiles = require('main-bower-files');
+
+    gulp.src(mainBowerFiles("**/*.js", {
+        "env": env,
+        "paths": rootPath
+    }))
+        .pipe(gulp.dest(jsDistAssetPath));
+
+    gulp.src(mainBowerFiles("**/*.css", {
+        "env": env,
+        "paths": rootPath
+    }))
+        .pipe(gulp.dest(cssDistAssetPath));
+
+    gulp.src(mainBowerFiles("**/fonts/**", {
+        "env": env,
+        "paths": rootPath
+    }))
+        .pipe(gulp.dest(fontsDistAssetPath));
+});
+
 gulp.task('watch', function () {
     var watch = require('gulp-watch'),
         batch = require('gulp-batch');
 
+    //twig
     watch(watchTwigSrc, batch(function (events, done) {
-        gulp.start('purge-dist', done);
         gulp.start('compile-twig', done);
     }));
 
-    //css
+    //libs
+    watch(watchBowerLibSrc, batch(function (events, done) {
 
-
-    //js
-
+    }));
 });
 
-gulp.task('default', ['purge-dist', 'compile-twig', 'watch']);
+gulp.task('build', ['compile-twig', 'main-bower-files']);
+gulp.task('default', ['purge-dist', 'build', 'watch']);
